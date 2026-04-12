d01378ed3de3d07d8c2916439e0b7acfacf0e56f
Mario Ventura <mario.ventura@godevel.com>
2026-04-12 20:45:10 +0200

    solve warnings in Client

--- PruebaTecnica/ClienteDesktop/Models/Client.cs
+++ PruebaTecnica/ClienteDesktop/Models/Client.cs
@@ -6,21 +6,21 @@ namespace ClienteDesktop.Models
     public class Client
     {
         [JsonProperty("dni")]
-        public string DNI { get; set; }
+        public string DNI { get; set; } = string.Empty;
 
         [JsonProperty("nombre")]
-        public string FirstName { get; set; }
+        public string FirstName { get; set; } = string.Empty;
 
         [JsonProperty("apellidos")]
-        public string LastName { get; set; }
+        public string LastName { get; set; } = string.Empty;
 
         [JsonProperty("fechaNacimiento")]
         public DateTime BirthDate { get; set; }
 
         [JsonProperty("telefono")]
-        public string Phone { get; set; }
+        public string Phone { get; set; } = string.Empty;
 
         [JsonProperty("email")]
-        public string Email { get; set; }
+        public string Email { get; set; } = string.Empty;
     }
 }
