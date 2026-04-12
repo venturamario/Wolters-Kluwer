9db18e25f38a52e8eb544eadfd15eace78dfaf19
Mario Ventura <mario.ventura@godevel.com>
2026-04-12 15:20:45 +0200

    ignore

--- PruebaTecnica/ClienteApi/obj/Debug/net10.0/ClienteApi.AssemblyInfo.cs
+++ PruebaTecnica/ClienteApi/obj/Debug/net10.0/ClienteApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClienteApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9555df692a1a212a27cdcb97194a6f34a2993e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9ccb960c7cf417890ebd943071a084d9c2d3b28")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClienteApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClienteApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- PruebaTecnica/ClienteDesktop/obj/Debug/net10.0-windows/ClienteDesktop.AssemblyInfo.cs
+++ PruebaTecnica/ClienteDesktop/obj/Debug/net10.0-windows/ClienteDesktop.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClienteDesktop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9555df692a1a212a27cdcb97194a6f34a2993e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9ccb960c7cf417890ebd943071a084d9c2d3b28")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClienteDesktop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClienteDesktop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
